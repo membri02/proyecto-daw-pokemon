61cebb57d61ddb1efc121d7b00e0e3742f887c75
Andres Membrives <andres@email.com>
2026-03-18 10:24:50 +0100

    Tienda terminada con sobres Premium y lógica base

--- app/Http/Controllers/TiendaController.php
+++ app/Http/Controllers/TiendaController.php
@@ -14,18 +14,39 @@ class TiendaController extends Controller
 
     public function abrirSobre($tipo)
     {
-        // 1. Extraemos las 5 cartas de la base de datos
-        $cartas = Carta::where('tipo', $tipo)
-                       ->inRandomOrder()
-                       ->limit(5)
-                       ->get();
+        $cartas = collect(); // Creamos una caja vacía para ir metiendo las cartas
 
-        // 2. Control de seguridad por si el sobre está vacío
-        if ($cartas->isEmpty()) {
-            return redirect('/sobres')->with('error', 'Aún no hay cartas de tipo ' . $tipo);
+        // LÓGICA SOBRES BÁSICOS (2 de su tipo + 3 al azar)
+        if (in_array($tipo, ['fuego', 'agua', 'planta'])) {
+            // Cogemos 2 cartas garantizadas del tipo seleccionado
+            $garantizadas = Carta::where('tipo', $tipo)->inRandomOrder()->limit(2)->get();
+            
+            // Cogemos 3 cartas de CUALQUIER tipo que no sean las que ya han tocado
+            $random = Carta::whereNotIn('id', $garantizadas->pluck('id'))->inRandomOrder()->limit(3)->get();
+            
+            $cartas = $garantizadas->concat($random);
+        } 
+        // LÓGICA SOBRE HOLO (1 Rara garantizada + 4 al azar)
+        elseif ($tipo === 'holo') {
+            $garantizada = Carta::where('rareza', 'Rara Holo')->inRandomOrder()->limit(1)->get();
+            $random = Carta::whereNotIn('id', $garantizada->pluck('id'))->inRandomOrder()->limit(4)->get();
+            $cartas = $garantizada->concat($random);
+        }
+        // LÓGICA SOBRE LEGENDARIO (1 Legendaria garantizada + 4 al azar)
+        elseif ($tipo === 'legendario') {
+            $garantizada = Carta::where('rareza', 'Legendaria ✨')->inRandomOrder()->limit(1)->get();
+            $random = Carta::whereNotIn('id', $garantizada->pluck('id'))->inRandomOrder()->limit(4)->get();
+            $cartas = $garantizada->concat($random);
         }
 
-        // 3. Pasamos las cartas y el tipo a una nueva vista
+        // Si la base de datos está vacía y devuelve 0 cartas, evitamos que explote
+        if ($cartas->count() < 5) {
+            return redirect('/sobres')->with('error', 'No hay suficientes cartas en la base de datos para abrir este sobre.');
+        }
+
+        // Barajamos las 5 cartas para que la garantizada no sea siempre la primera
+        $cartas = $cartas->shuffle();
+
         return view('tienda.apertura', compact('cartas', 'tipo'));
     }
 }
--- database/seeders/CartasSeeder.php
+++ database/seeders/CartasSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Carta; 
+use App\Models\Carta;
 
 class CartasSeeder extends Seeder
 {
@@ -30,6 +30,25 @@ class CartasSeeder extends Seeder
             ['nombre' => 'Venusaur', 'tipo' => 'planta', 'rareza' => 'Rara Holo', 'imagen_url' => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png'],
             ['nombre' => 'Oddish', 'tipo' => 'planta', 'rareza' => 'Común', 'imagen_url' => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/43.png'],
             ['nombre' => 'Scyther', 'tipo' => 'planta', 'rareza' => 'Legendaria ✨', 'imagen_url' => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/123.png'],
+
+            // NUEVAS: CARTAS ELÉCTRICAS
+            ['nombre' => 'Pikachu', 'tipo' => 'electrico', 'rareza' => 'Infrecuente', 'imagen_url' => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png'],
+            ['nombre' => 'Voltorb', 'tipo' => 'electrico', 'rareza' => 'Común', 'imagen_url' => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/100.png'],
+            ['nombre' => 'Zapdos', 'tipo' => 'electrico', 'rareza' => 'Legendaria ✨', 'imagen_url' => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/145.png'],
+            ['nombre' => 'Magnemite', 'tipo' => 'electrico', 'rareza' => 'Común', 'imagen_url' => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/81.png'],
+            ['nombre' => 'Electabuzz', 'tipo' => 'electrico', 'rareza' => 'Rara Holo', 'imagen_url' => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/125.png'],
+
+            // NUEVAS: CARTAS NORMALES
+            ['nombre' => 'Pidgey', 'tipo' => 'normal', 'rareza' => 'Común', 'imagen_url' => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/16.png'],
+            ['nombre' => 'Snorlax', 'tipo' => 'normal', 'rareza' => 'Rara Holo', 'imagen_url' => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/143.png'],
+            ['nombre' => 'Eevee', 'tipo' => 'normal', 'rareza' => 'Infrecuente', 'imagen_url' => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/133.png'],
+            ['nombre' => 'Rattata', 'tipo' => 'normal', 'rareza' => 'Común', 'imagen_url' => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/19.png'],
+            ['nombre' => 'Meowth', 'tipo' => 'normal', 'rareza' => 'Común', 'imagen_url' => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/52.png'],
+            
+            // NUEVAS: EXTRAS PARA ASEGURAR STOCK DE PREMIUM
+            ['nombre' => 'Mewtwo', 'tipo' => 'psiquico', 'rareza' => 'Legendaria ✨', 'imagen_url' => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/150.png'],
+            ['nombre' => 'Mew', 'tipo' => 'psiquico', 'rareza' => 'Legendaria ✨', 'imagen_url' => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png'],
+            ['nombre' => 'Alakazam', 'tipo' => 'psiquico', 'rareza' => 'Rara Holo', 'imagen_url' => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/65.png'],
         ];
 
         Carta::insert($cartas);
