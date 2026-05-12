ade5ed4b2c761b1eaedb522aa212a5ef052b679c
Andres Membrives <andres.membrives@educa.madrid.org>
2026-05-12 16:00:52 +0200

    cambio en el icono

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Proyecto TCG DAW</title>
-    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}">
     <x-pokemon-styles />
 </head>
 <body style="background-color: var(--bg-color); color: var(--text-main); margin: 0; padding: 0;">
